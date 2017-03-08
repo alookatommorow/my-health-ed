c03ee37d2c24c3e4e49fc6a185dcf942aff974ab
alookatommorow <hessjohnt@gmail.com>
2017-03-07 16:00:37 -0800

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-girls = Perspective.create({ name: "girls" })
-guys = Perspective.create({ name: "guys" })
-parents = Perspective.create({ name: "parents" })
-other = Perspective.create({ name: "other" })
+girls = Perspective.create!({ name: "girls" })
+guys = Perspective.create!({ name: "guys" })
+parents = Perspective.create!({ name: "parents" })
+other = Perspective.create!({ name: "other" })
+
+p other
+
+girl_story = Story.create!({ perspective: girls, title: "girl story" })
+guy_story = Story.create!({ perspective: guys, title: "guy story" })
+parent_story = Story.create!({ perspective: parents, title: "parent story" })
+other_story = Story.create!({ perspective: other, title: "other story" })
+
+p girl_story
