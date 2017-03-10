a85c8a60a1c23eab1a320b5bc2052ead4c264feb
alookatommorow <hessjohnt@gmail.com>
2017-03-09 17:38:22 -0800

    add comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,14 @@ class CommentsController < ApplicationController
 	def index
 		@comments = Comment.where(story_id: params[:story_id])
 	end
+
+	def create
+		Comment.create(comment_params)
+	end
+
+	private
+
+	  def comment_params
+	    params.require(:comment).permit(:body, :story_id)
+	  end
 end
