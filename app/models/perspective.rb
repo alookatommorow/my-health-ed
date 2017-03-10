aec2d056e593aac66bede16b15b9586af077cf63
alookatommorow <hessjohnt@gmail.com>
2017-03-09 17:51:29 -0800

    add dependent destory to models

--- app/models/perspective.rb
+++ app/models/perspective.rb
@@ -1,6 +1,6 @@
 class Perspective < ActiveRecord::Base
-	has_many :stories
-	has_many :comments
+	has_many :stories, dependent: :destroy
+	has_many :comments, dependent: :destroy
 	validates :name, presence: true, uniqueness: true
 
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,6 @@
 class Story < ActiveRecord::Base
 	belongs_to :perspective
-	has_many :story_texts
-	has_many :comments
+	has_many :story_texts, dependent: :destroy
+	has_many :comments, dependent: :destroy
 
 end
