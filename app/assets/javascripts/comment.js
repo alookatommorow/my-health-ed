f404cecdc7f4c908f5d7dcfac19daba709ca7523
alookatommorow <hessjohnt@gmail.com>
2017-03-11 12:31:41 -0800

    revise layout

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -8,8 +8,20 @@ document.addEventListener("turbolinks:load", function() {
     $.post(url, data, callback);
   })
 
+  $("#comment-button").click(function() {
+    $("#comment-form-container").slideToggle();
+  });
+
+  $(".cancel-button").click(function() {
+    $("#comment-form-container").slideToggle();
+  });
+
   function renderResponse(response) {
     var container = $(this).data("ajax-submit");
     $("[data-ajax-replace="+container+"]").append(response);
+    $("#comment-form-container").slideToggle(function() {
+      $(this).find(".comment-submit-button").prop("disabled", false);
+      $(this).find("textarea").val("");
+    })
   }
 });
